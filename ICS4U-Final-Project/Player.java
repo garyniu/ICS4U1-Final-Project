061216982ca2c3d8ac829fffe0da70a51cc403b2
Justin <sinyattin@gmail.com>
2023-01-17 14:21:18 -0500

    Scuffed but working Player class
    wasd for movement, arrow keys for attacking

--- ICS4U-Final-Project/Player.java
+++ ICS4U-Final-Project/Player.java
@@ -13,78 +13,225 @@ public class Player extends FreeMovement
     //Sprites(Justin)
     //Items(Victor)
     //Movement(Gary)
-    
-    private int timer = 0;
-    
-    
+
     //charater animation images
     private GreenfootImage[] upImages;
     private GreenfootImage[] downImages;
     private GreenfootImage[] rightImages;
     private GreenfootImage[] leftImages;
-    
+    //character attack animation images
+    private GreenfootImage[] aupImages;
+    private GreenfootImage[] adownImages;
+    private GreenfootImage[] arightImages;
+    private GreenfootImage[] aleftImages;
+
     //animation image speed
     private SimpleTimer actionTimer;
     private int delay = 0;
     private boolean isFacingRight = true;
     private boolean isFacingUp = true;
-    
-    private int size = 75;//quick way to adjust all the MainCharater's image sizes
-    private int CharaterSpeed = 2;//quick way to adjust all the MainCharater's speed
-    private int SprintSpeed = 5;
-    
+
+    private int size = 60;//quick way to adjust all the MainCharater's image sizes
+    private int atkSize = 180;
+    private int CharacterSpeed = 1;//quick way to adjust all the MainCharater's speed
+    private int SprintSpeed = 2;//quick way to adjust all the MainCharater's sprint speed
+
     
     public Player(int x, int y){
         super(x, y);
+        //walk
         upImages = new GreenfootImage[9];
         downImages = new GreenfootImage[9];
         rightImages = new GreenfootImage[9];
         leftImages = new GreenfootImage[9];
-        
+        //attack
+        aupImages = new GreenfootImage[6];
+        adownImages = new GreenfootImage[6];
+        arightImages = new GreenfootImage[6];
+        aleftImages = new GreenfootImage[6];
+
+        //walk
         for(int i = 0; i < downImages.length; i++)//main charater walking down animation 
         {
-             downImages[i] = new GreenfootImage("images/animate1/down" + i + ".png");
-             downImages[i].scale(size, size);
+            downImages[i] = new GreenfootImage("images/walk/down/down" + i + ".png");
+            downImages[i].scale(size, size);
         }
-        
-        for(int i = 0; i < downImages.length; i++)//main charater walking up animation 
+        for(int i = 0; i < upImages.length; i++)//main charater walking up animation 
         {
-             upImages[i] = new GreenfootImage("images/animate3/up" + i + ".png");
-             upImages[i].scale(size, size);
+            upImages[i] = new GreenfootImage("images/walk/up/up" + i + ".png");
+            upImages[i].scale(size, size);
         }
-        
         for(int i = 0; i < rightImages.length; i++)//main charater walking left and right animation 
         {
-             rightImages[i] = new GreenfootImage("images/animate2/right" + i + ".png");
-             rightImages[i].scale(size, size);
-             
-             leftImages[i] = new GreenfootImage("images/animate2/right" + i + ".png");
-             leftImages[i].mirrorHorizontally();
-             leftImages[i].scale(size, size);
+            rightImages[i] = new GreenfootImage("images/walk/right/right" + i + ".png");
+            rightImages[i].scale(size, size);
+
+            leftImages[i] = new GreenfootImage("images/walk/right/right" + i + ".png");
+            leftImages[i].mirrorHorizontally();
+            leftImages[i].scale(size, size);
+        }
+
+        //attack
+        for(int i = 0; i < adownImages.length; i++)//main charater walking down animation 
+        {
+            adownImages[i] = new GreenfootImage("images/attack/down/down" + i + ".png");
+            adownImages[i].scale(atkSize, atkSize);
+        }
+        for(int i = 0; i < aupImages.length; i++)//main charater walking up animation 
+        {
+            aupImages[i] = new GreenfootImage("images/attack/up/up" + i + ".png");
+            aupImages[i].scale(atkSize, atkSize);
+        }
+        for(int i = 0; i < arightImages.length; i++)//main charater walking left and right animation 
+        {
+            arightImages[i] = new GreenfootImage("images/attack/right/right" + i + ".png");
+            arightImages[i].scale(atkSize, atkSize);
+
+            aleftImages[i] = new GreenfootImage("images/attack/right/right" + i + ".png");
+            aleftImages[i].mirrorHorizontally();
+            aleftImages[i].scale(atkSize, atkSize);
         }
         setImage(rightImages[0]);
-
+        
         actionTimer = new SimpleTimer();
         actionTimer.mark();
-        
     }
-    
-    public void act()
+
+    int curIndex = 0;
+    public void idleAction()//controls the animation image speed for moving left and right
     {
-        timer++;
-        super.act();
-        
-        
-        if (Greenfoot.isKeyDown("w")){
-            moveUp();
-        } else if (Greenfoot.isKeyDown("s")){
-            moveDown();
-        } else if (Greenfoot.isKeyDown("a")){
-            moveLeft();
-        } else if (Greenfoot.isKeyDown("d")){
-            moveRight();
+        if(actionTimer.millisElapsed() > 100)
+        {
+            if (isFacingRight)
+            {
+                setImage(rightImages[curIndex]);
+            }
+            else
+            {
+                setImage(leftImages[curIndex]);
+            }
+
+            curIndex++;
+            if(curIndex == 6){
+                curIndex = 0;
+            }
+            actionTimer.mark();
         }
     }
-    
-    
+
+    public void idleActionTwo()//controls the animation image speed for moving up and down
+    {
+        if(actionTimer.millisElapsed() > 100)
+        {
+
+            if (isFacingUp)
+            {
+                setImage(upImages[curIndex]);
+            }
+            else
+            {
+                setImage(downImages[curIndex]);
+            }
+            curIndex++;
+            if(curIndex == 6){
+                curIndex = 0;
+            }
+            actionTimer.mark();
+        }
+    }
+
+    public void verticalAttack(){
+        if(actionTimer.millisElapsed() > 100)
+        {
+
+            if (isFacingUp)
+            {
+                setImage(aupImages[curIndex]);
+            }
+            else
+            {
+                setImage(adownImages[curIndex]);
+            }
+            curIndex++;
+            if(curIndex == 6){
+                curIndex = 0;
+            }
+            actionTimer.mark();
+        }
+    }
+
+    public void horizontalAttack(){
+        if(actionTimer.millisElapsed() > 100)
+        {
+            if (isFacingRight)
+            {
+                setImage(arightImages[curIndex]);
+            }
+            else
+            {
+                setImage(aleftImages[curIndex]);
+            }
+
+            curIndex++;
+            if(curIndex == 6){
+                curIndex = 0;
+            }
+            actionTimer.mark();
+        }
+    }
+
+    public void act()
+    {
+        super.act();
+
+        //movement
+        if(Greenfoot.isKeyDown("d")){//MainCharater moves right
+            isFacingRight = true;
+            moveRight();
+            idleAction();
+        }else if(Greenfoot.isKeyDown("a")){//MainCharater moves left
+            isFacingRight = false;
+            moveLeft();
+            idleAction();
+        }else if(Greenfoot.isKeyDown("w")){//MainCharater moves up
+            int x = getX();
+            int y = getY();
+            moveUp();
+
+            isFacingUp = true;
+            idleActionTwo();
+        }else if(Greenfoot.isKeyDown("s")){//MainCharater moves down
+            int x = getX();
+            int y = getY();
+            moveDown();
+            
+            isFacingUp = false;
+            idleActionTwo();
+        }
+        
+        //attack
+        if(Greenfoot.isKeyDown("right")){//MainCharater moves right
+            isFacingRight = true;
+            horizontalAttack();
+            idleAction();
+        }else if(Greenfoot.isKeyDown("left")){//MainCharater moves left
+            isFacingRight = false;
+            horizontalAttack();
+            idleAction();
+        }else if(Greenfoot.isKeyDown("up")){//MainCharater moves up
+            int x = getX();
+            int y = getY();
+            verticalAttack();
+
+            isFacingUp = true;
+            idleActionTwo();
+        }else if(Greenfoot.isKeyDown("down")){//MainCharater moves down
+            int x = getX();
+            int y = getY();
+            verticalAttack();
+            
+            isFacingUp = false;
+            idleActionTwo();
+        }
+    }
 }
