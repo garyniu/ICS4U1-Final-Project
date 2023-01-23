d0e3b0db5840e2a9156b4de6b140c56317a0ad62
Xu <101126520+LeoX88886@users.noreply.github.com>
2023-01-23 13:58:06 -0500

    added SmallWall

---
+++ ICS4U-Final-Project/SmallWall.java
@@ -0,0 +1,64 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Wall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SmallWall extends Others
+{
+    private GreenfootImage playImage = new GreenfootImage("images/misc/isowall24.png");
+    
+    private GreenfootImage hiddenImage = new GreenfootImage("images/misc/hWall.png");
+    
+    private GreenfootImage semihiddenImage = new GreenfootImage("images/misc/isowall23.png");
+    /**
+     * Act - do whatever the Wall wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public SmallWall() 
+    {
+        // Add your action code here.
+        change1();
+    }
+    
+    public void addedToWorld(World GameWorld){
+        //System.out.println("wall pos: " + getX() + " " + getY());
+        
+        xg = getX();
+        yg = getY();
+    }
+    
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("1"))
+        {
+            change1();
+        }
+        if(Greenfoot.isKeyDown("2"))
+        {
+            change2();
+        }
+        if(Greenfoot.isKeyDown("3"))
+        {
+            change3();
+        }
+    }
+    
+    public void change1()
+    {
+        setImage(playImage);
+    }
+    
+    public void change2()
+    {
+        setImage(hiddenImage);
+    }
+    
+    public void change3()
+    {
+        setImage(semihiddenImage);
+    }
+}
--- ICS4U-Final-Project/SpiderWorld.java
+++ ICS4U-Final-Project/SpiderWorld.java
@@ -12,6 +12,25 @@ public class SpiderWorld extends World
     
     private WorldBackground wbg3;
     
+    
+    
+    public int[][] mapFour={
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1},
+        }; 
+        
+        
+    public Wall[][] wallListFour = new Wall[10][7];
+    
+    
     private int xd = 0, yd = 0;
     /**
      * Constructor for objects of class SpookyWorld.
@@ -39,6 +58,23 @@ public class SpiderWorld extends World
         
         xd = playerTest.getX();
         yd = playerTest.getY();
+        
+        for(int x = 0; x < 10; x++){
+            for(int y = 0; y < 7; y++){//for each cell in the array
+                //depending on the cell code, add/initialize objects (see map code at top for reference of what codes correspond to what objects
+               
+                if(mapFour[x][y]==1){
+                    wallListFour[x][y]=new Wall();
+                    //addObject(wallListFour[x][y], (400 - x * 14 + y * 14-1), (140 + x * 7 + y * 7));
+                    
+                    //addObject(wallListFour[x][y], (440- x * 14 + y * 14-1), (125 + x * 7 + y * 7));
+                    
+                    //X values: higher the value the more it shifts right, Y values: the higher the more the it shifts down
+                    
+                }
+                
+            }
+        }
     }
     
     public void act()
