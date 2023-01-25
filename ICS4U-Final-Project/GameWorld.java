272b261201ed19e284a1d81b289fb911e679dbdc
LeoX88886 <101126520+LeoX88886@users.noreply.github.com>
2023-01-25 10:21:29 -0500

    added GameCover1

--- ICS4U-Final-Project/GameWorld.java
+++ ICS4U-Final-Project/GameWorld.java
@@ -121,7 +121,7 @@ public class GameWorld extends World
         // Create a new world with 720x405 cells with a cell size of 1x1 pixels.
         super(600, 500, 1, false);
 
-        setPaintOrder(Fog.class, Boundary.class, SuperStatBar.class, PlayerHitbox.class, Player.class, BossEnemy.class, Enemy.class, Portal.class, EnterPortal.class, Wall.class, WorldBackground.class);
+        setPaintOrder(Fog.class, Boundary.class, GameCover1.class, SuperStatBar.class, PlayerHitbox.class, Player.class, BossEnemy.class, Enemy.class, Portal.class, EnterPortal.class, Wall.class, WorldBackground.class);
 
         wbg = new WorldBackground(background);
 
@@ -157,6 +157,11 @@ public class GameWorld extends World
         addObject(playerTest2, this.getWidth()/2, this.getHeight()/2);
 
         addObject(wbg, this.getWidth()/2, this.getHeight()/2);
+        
+        //GameCover
+        GameCover1 c1 = new GameCover1();
+        addObject(c1, 145, 355);
+        
         
         //Boudary class
         Boundary bound1 = new Boundary(550,50);
@@ -225,7 +230,7 @@ public class GameWorld extends World
     
     //update player hp bar
     public static void updateHP(int hp){
-        health.update((int)(hp));
+        //health.update((int)(hp));
     }
     //update stamina bar
     /*public static void updateStamina(int st){
